8442dcb041bfb97ac5dea84a831c15196aa6b0e8
Artem <artemmz@inbox.ru>
2021-11-07 20:43:32 +0300

    add second link in messenger

--- messanger/js.js
+++ messanger/js.js
@@ -1,17 +1,41 @@
 const submit = document.getElementById("submit");
+const submit2 = document.getElementById("submit2");
 
-submit.addEventListener("click", messageW = () => {
-    let messageText = document.getElementById("link").value;
-    let messengerDisplay = document.getElementById("messengerDisplay");
-
-    messengerDisplay.insertAdjacentHTML("beforeend", `<div class="message"> ${messageText} </div>`);
+submit.addEventListener("click", () => {
     
+    const messageText = document.getElementById("link");
+    if (messageText.value.length > 0) {
+        const messengerDisplay = document.getElementById("messengerDisplay");
+        
+        messengerDisplay.insertAdjacentHTML("beforeend", `<div class="message"> ${messageText.value} </div>`);
+        messageText.value = "";
+    }
     console.log(messageText);
 });
 
-submit.addEventListener("mousedown", mouseDown = () => {
-    let submit = document.getElementsByClassName("messageSubmit");
-    submit = document.getElementsByClassName("messageSubmitMousedown");
-    console.log("hi boy")
+submit2.addEventListener("click" , () => {
+    const messageText = document.getElementById("link2");
+
+    if (messageText.value.length > 0) {
+        const messengerDisplay = document.getElementById("messengerDisplay");
+
+        messengerDisplay.insertAdjacentHTML("beforeend", `<div class="message align_self_end"> ${messageText.value} </div>`);
+        messageText.value = ""
+        console.log("program is work")
+    }
+});
+
+submit.addEventListener("mousedown", () => {
+    // const submit = document.getElementsByClassName("messageSubmit");
+    // submit = document.getElementsByClassName("messageSubmitMousedown");
+    submit.classList.add("green");
+
+    console.log(submit.classList);
 })
 
+submit.addEventListener("mouseup", () => {
+    submit.classList.remove("green");
+    console.log(submit.classList);
+})
+
+// const message = document.getElementsByClassName()
